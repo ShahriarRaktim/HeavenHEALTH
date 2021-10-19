2111e4e004ad0c578be7c375a8d0145c7c872901
ShahriarRaktim <shahriarahmedraktim@gmail.com>
2021-10-19 13:12:58 +0600

    all service

--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Card, Col } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
-const Service = () => {
+const Service = ({service}) => {
+    const {name, summary, img} = service;
+    console.log(name)
     return (
-        <div>
-            
-        </div>
+        <Col>
+        <Card className="m-3 card">
+          <Card.Img variant="top" src={img} />
+          <Card.Body>
+            <Card.Title className="name">{name}</Card.Title>
+            <Card.Text>
+              {summary}
+            </Card.Text>
+            <NavLink to="/details" className="contact">
+            Details <i class="fas fa-arrow-alt-circle-right"></i>
+          </NavLink>
+          </Card.Body>
+          
+        </Card>
+      </Col>
     );
 };
 
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,14 +1,47 @@
-import React from 'react';
-import './Services.css'
-import service from '../../image/service.jpg'
+import React, { useEffect, useState } from "react";
+import "./Services.css";
+import service from "../../image/service.jpg";
+import { Row } from "react-bootstrap";
+import Service from "../Service/Service";
 
 const Services = () => {
-    return (
-        <div className="main-services">
-            <img  src={service} alt="" />
-            <div><h2>hello</h2></div>
+    const [services, setServices] = useState([])
+    
+    useEffect(()=>{
+        fetch("allservices.json")
+        .then(res => res.json())
+        .then(data => setServices(data))
+    },[])
+  return (
+      <>
+    <div className="main-services">
+      <div className="service-banar">
+        {" "}
+        <h1>
+          <span>HERE</span> The best services that we provided
+        </h1>
+      </div>
+      <img src={service} alt="" />
+    </div>
+    <section>
+        <div className="services">
+          <h1>Medical Service</h1>
+          <h2>HeavenHEALTH Services</h2>
+          <p>
+            As you continue to keep tabs on the <span>HeavenHEALTH</span>{" "}
+            industry, don’t forget passion you’ve
+            been feeling to propel your career forward.
+          </p>
         </div>
-    );
+        <Row xs={1} md={2} lg={3} className="g-4">
+          {services.map((service) => <Service
+          key={service.id}
+          service={service}
+          ></Service>)}
+        </Row>
+    </section>
+    </>
+  );
 };
 
 export default Services;
