80d1f4d41fa56fa11a2a0ee0471c33ed412fc5b6
ShahriarRaktim <shahriarahmedraktim@gmail.com>
2021-10-20 22:57:12 +0600

    Ready to Use

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -4,31 +4,31 @@ import { NavLink } from "react-router-dom";
 import useAuth from "../../Hooks/useAuth";
 
 const Register = () => {
-  const { googleSignIn, register, user} = useAuth();
+  const { googleSignIn, register, user } = useAuth();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("")
+  const [error, setError] = useState("");
 
   const handleUserEmail = (e) => {
     setEmail(e.target.value);
   };
 
   const handleUserPassword = (e) => {
-    const regix = /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!#$%&?]{8,20}$/;
-    if(!regix.test(e.target.value) ){
-      setError("Sir ! You have to use at least 8 characters with at least one uppercase, one lowercase, one digit, one spcial character in your password but not more than 20 characters");
-    }
-    else{
+    const regix =
+      /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!#$%&?]{8,20}$/;
+    if (!regix.test(e.target.value)) {
+      setError(
+        "Sir, It's Weak Password ! You have to use at least 8 characters with  uppercase, lowercase, digit, spcial character in your password but not more than 20 characters.Make it more complex"
+      );
+    } else {
       setPassword(e.target.value);
     }
-    
   };
-  
 
   const handleCreateAccount = (e) => {
     register(email, password);
     e.preventDefault();
-    console.log('ok', error)
+    console.log("ok", error);
   };
 
   return (
@@ -37,10 +37,8 @@ const Register = () => {
         {!user.email ? (
           <>
             <h1>Please Register</h1>
-            {
-              error && <h5 className="text-danger" >{error}</h5>
-            }
-            
+            {error && <h5 className="text-danger">{error}</h5>}
+
             <form onSubmit={handleCreateAccount}>
               <div className="input">
                 <input
